c429591018d049455a44cade33f3ba3cf4ba3d9e
juzybits <96890594+juzybits@users.noreply.github.com>
2024-01-16 20:43:30 -0100

    move here ADDRESS_REGEX, shortenSuiAddress, makeSuiExplorerUrl from @polymedia/webutils

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,12 @@
+/**
+ * A Sui network name
+ */
 export type NetworkName =  'mainnet' | 'testnet' | 'devnet' | 'localnet';
+
+/**
+ * A Sui Explorer item type, as in:
+ * https://suiexplorer.com/address/...
+ * https://suiexplorer.com/object/...
+ * https://suiexplorer.com/package/...
+ */
+export type SuiExplorerItem = 'address' | 'object' | 'package';
