361040ad40bdbb44d49a49b1aaf5dfc0ec510022
juzybits <96890594+juzybits@users.noreply.github.com>
2024-02-18 23:13:56 -0100

    SuiMultiClient: refine RPCs for mainnet/testnet/devnet

--- src/SuiMultiClient.ts
+++ src/SuiMultiClient.ts
@@ -143,34 +143,44 @@ export type SuiClientWithEndpoint = SuiClient & {
 export const RPC_ENDPOINTS: Record<NetworkName, string[]> = {
     'mainnet': [
         getFullnodeUrl('mainnet'),
-        // 'https://mainnet-rpc.sui.chainbase.online',          // 567 response
-        // 'https://mainnet.sui.rpcpool.com',                   // 403 forbidden when using VPN
         'https://mainnet.suiet.app',
         'https://rpc-mainnet.suiscan.xyz',
         'https://sui-mainnet-endpoint.blockvision.org',
         'https://sui-mainnet.public.blastapi.io',
-        // 'https://sui-rpc-mainnet.testnet-pride.com',         // 502 bad gateway
-        // 'https://sui1mainnet-rpc.chainode.tech',             // 502 bad gateway
-        // 'https://sui-mainnet-ca-1.cosmostation.io',
         'https://sui-mainnet-ca-2.cosmostation.io',
-        // 'https://sui-mainnet-eu-1.cosmostation.io',          // 000
-        // 'https://sui-mainnet-eu-2.cosmostation.io',          // 000
         'https://sui-mainnet-eu-3.cosmostation.io',
         'https://sui-mainnet-eu-4.cosmostation.io',
         'https://sui-mainnet-us-1.cosmostation.io',
         'https://sui-mainnet-us-2.cosmostation.io',
-        // 'https://sui-mainnet-rpc.allthatnode.com',           // 429 too many requests
-        // 'https://sui-mainnet-rpc-germany.allthatnode.com',   // 429 too many requests
-        // 'https://sui-mainnet-rpc-korea.allthatnode.com',     // too slow/far
-        // 'https://sui-mainnet.nodeinfra.com',                 // 429 too many requests
         'https://sui.publicnode.com',
         'https://sui-mainnet-rpc.bartestnet.com',
+
+        // 'https://mainnet.sui.rpcpool.com',                   // 403 forbidden when using VPN
+        'https://sui1mainnet-rpc.chainode.tech',             // 502 bad gateway (works now)
+
+        // 'https://mainnet-rpc.sui.chainbase.online',          // 567 response occasionally
+        // 'https://sui-mainnet-rpc-germany.allthatnode.com',   // 429 too many requests
+        // 'https://sui-mainnet-rpc-korea.allthatnode.com',     // 429 too many requests
+        // 'https://sui-mainnet-rpc.allthatnode.com',           // 429 too many requests
+        // 'https://sui-mainnet.nodeinfra.com',                 // 429 too many requests
+
+        // 'https://sui-mainnet-ca-1.cosmostation.io',          // 404
+        // 'https://sui-rpc-mainnet.testnet-pride.com',         // 502 bad gateway
+        // 'https://sui-mainnet-eu-1.cosmostation.io',          // 000
+        // 'https://sui-mainnet-eu-2.cosmostation.io',          // 000
     ],
     'testnet': [
         getFullnodeUrl('testnet'),
+        'https://testnet.suiet.app',
+        'https://rpc-testnet.suiscan.xyz',
+        'https://sui-testnet-endpoint.blockvision.org',
+        'https://sui-testnet.public.blastapi.io',
+        // 'https://testnet.sui.rpcpool.com',                   // 403 forbidden when using VPN
+        // 'https://sui-testnet.nodeinfra.com',                 // 429 too many requests
     ],
     'devnet': [
         getFullnodeUrl('devnet'),
+        'https://devnet.suiet.app',
     ],
     'localnet': [
         // to simulate multiple RPC endpoints locally
