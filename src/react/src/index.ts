034578852702ab86bde2fdc0ffc12526394f8ff2
juzybits <96890594+juzybits@users.noreply.github.com>
2024-05-23 13:26:42 +0000

    react: remove rpcConfig

--- src/react/src/index.ts
+++ src/react/src/index.ts
@@ -3,5 +3,4 @@ export * from "./Modal";
 export * from "./NetworkSelector";
 export * from "./makeCssUrl";
 export * from "./network";
-export * from "./rpcConfig";
 export * from "./useClickOutside";
