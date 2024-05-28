16a5bc4d7f4f8570819d28c431d888f0d4787a27
juzybits <96890594+juzybits@users.noreply.github.com>
2024-05-28 13:10:33 +0000

    rpcs: select working mainnet RPCs

--- src/core/src/rpcs.ts
+++ src/core/src/rpcs.ts
@@ -3,33 +3,32 @@ import { NetworkName } from "./types";
 
 /**
  * Default RPC endpoint URLs for SuiMultiClient.
- * Manually tested for the last time on January 2023.
+ * Manually tested for the last time on May 2024.
  */
 export const RPC_ENDPOINTS: Record<NetworkName, string[]> = {
     "mainnet": [
-        getFullnodeUrl("mainnet"),
+        getFullnodeUrl("mainnet").replace(/:\d+/, ''),
         "https://mainnet.suiet.app",
         "https://rpc-mainnet.suiscan.xyz",
-        "https://sui-mainnet-ca-2.cosmostation.io",
+        'https://mainnet.sui.rpcpool.com',
+        "https://sui-mainnet.nodeinfra.com",
+        "https://sui1mainnet-rpc.chainode.tech",
+        "https://sui-mainnet.public.blastapi.io",
+        'https://mainnet-rpc.sui.chainbase.online',
         "https://sui-mainnet-endpoint.blockvision.org",
+        "https://sui-mainnet-ca-2.cosmostation.io",
         "https://sui-mainnet-eu-3.cosmostation.io",
         "https://sui-mainnet-eu-4.cosmostation.io",
-        "https://sui-mainnet-rpc.bartestnet.com",
         "https://sui-mainnet-us-1.cosmostation.io",
         "https://sui-mainnet-us-2.cosmostation.io",
-        "https://sui-mainnet.public.blastapi.io",
-        "https://sui.publicnode.com",
-
-        "https://sui-mainnet-rpc-germany.allthatnode.com",
         "https://sui-mainnet-rpc.allthatnode.com",
-        "https://sui-mainnet.nodeinfra.com",                    // 429 too many requests (occasionally)
-        "https://sui1mainnet-rpc.chainode.tech",                // 502 bad gateway (works now)
-        // 'https://mainnet.sui.rpcpool.com',                   // 403 forbidden when using VPN
-        // 'https://sui-mainnet-rpc-korea.allthatnode.com',     // too slow/far
+        'https://sui-mainnet-rpc-korea.allthatnode.com',
+        "https://sui-mainnet-rpc-germany.allthatnode.com",
 
-        // 'https://mainnet-rpc.sui.chainbase.online',          // 567 response
-        // 'https://sui-mainnet-ca-1.cosmostation.io',          // 404
-        // 'https://sui-rpc-mainnet.testnet-pride.com',         // 502 bad gateway
+        // "https://sui.publicnode.com",                        // CORS error
+        // "https://sui-mainnet-rpc.bartestnet.com",            // 403
+        // 'https://sui-rpc-mainnet.testnet-pride.com',         // 502
+        // 'https://sui-mainnet-ca-1.cosmostation.io',          // 502
         // 'https://sui-mainnet-eu-1.cosmostation.io',          // 000
         // 'https://sui-mainnet-eu-2.cosmostation.io',          // 000
     ],
