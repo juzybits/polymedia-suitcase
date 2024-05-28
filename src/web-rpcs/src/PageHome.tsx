d96a82065bbe2fdc1c4e173edf938ab665913f58
juzybits <96890594+juzybits@users.noreply.github.com>
2024-05-28 11:27:38 +0000

    rpcs: create web app scaffolding

---
+++ src/web-rpcs/src/PageHome.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const PageHome: React.FC = () =>
+{
+    /* State */
+
+    /* Functions */
+
+    /* HTML */
+
+    return <>
+
+    <h1><span className="rainbow">Sui RPC test</span></h1>
+
+    </>;
+};
---
+++ src/web-rpcs/src/PageNotFound.tsx
@@ -0,0 +1,7 @@
+export const PageNotFound: React.FC = () =>
+{
+    return <>
+        <h1><span className="rainbow">404</span></h1>
+        <p>Page not found.</p>
+    </>;
+};
