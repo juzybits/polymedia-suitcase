7e18d5b428901a6f34f874895217d3d48f583eb4
juzybits <96890594+juzybits@users.noreply.github.com>
2024-09-06 20:38:48 +0000

    core: add parseTxError()

--- src/core/src/transactions.ts
+++ src/core/src/transactions.ts
@@ -153,6 +153,41 @@ export function txResToData(
 }
 
 /**
+ * Parse a Move abort string (from `SuiTransactionBlockResponse.effects.status.error`)
+ * into its different parts.
+ *
+ * Based on `sui/crates/sui/src/clever_error_rendering.rs`.
+ *
+ * Example error string:
+ * `MoveAbort(MoveLocation { module: ModuleId { address: 0x123, name: Identifier("the_module") }, function: 1, instruction: 29, function_name: Some("the_function") }, 5008) in command 2`
+ */
+export function parseTxError(
+    error: string,
+): {
+    packageId: string;
+    module: string;
+    function: string;
+    instruction: number;
+    code: number;
+    command: number;
+} | null
+{
+    const match = error.match(/MoveAbort.*address:\s*(.*?),.* name:.*Identifier\((.*?)\).*instruction:\s+(\d+),.*function_name:.*Some\((.*?)\).*},\s*(\d+).*in command\s*(\d+)/);
+    if (!match) {
+        return null;
+    }
+    const cleanString = (s: string) => s.replace(/\\/g, "").replace(/"/g, "");
+    return {
+        packageId: match[1],
+        module: cleanString(match[2]),
+        instruction: parseInt(match[3]),
+        function: cleanString(match[4]),
+        code: parseInt(match[5]),
+        command: parseInt(match[6]),
+    };
+}
+
+/**
  * Build transactions for the `sui::transfer` module.
  */
 export const TransferModule =
