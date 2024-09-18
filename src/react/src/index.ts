78f95409d74a457ec906886603a8b4373fe73279
juzybits <96890594+juzybits@users.noreply.github.com>
2024-09-18 12:58:44 +0000

    react: Links.tsx -> links.tsx

--- src/react/src/index.ts
+++ src/react/src/index.ts
@@ -1,6 +1,6 @@
-export * from "./Links";
 export * from "./Modal";
 export * from "./NetworkSelector";
+export * from "./links";
 export * from "./makeCssUrl";
 export * from "./network";
 export * from "./types";
