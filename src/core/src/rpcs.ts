df74c935d83461dda120193ab829aa4078a5876f
juzybits <96890594+juzybits@users.noreply.github.com>
2024-09-23 19:02:07 +0000

    core: update RPC list

--- src/core/src/rpcs.ts
+++ src/core/src/rpcs.ts
@@ -3,11 +3,11 @@ import { NetworkName } from "./types.js";
 
 /**
  * Default RPC endpoint URLs for SuiMultiClient.
- * Manually tested for the last time on May 2024.
+ * Manually tested for the last time on September 2024.
  */
 export const RPC_ENDPOINTS: Record<NetworkName, string[]> = {
     "mainnet": [
-        getFullnodeUrl("mainnet").replace(/:\d+/, ""),
+        removePort(getFullnodeUrl("mainnet")),
         "https://mainnet.suiet.app",
         "https://rpc-mainnet.suiscan.xyz",
         "https://mainnet.sui.rpcpool.com",
@@ -17,32 +17,32 @@ export const RPC_ENDPOINTS: Record<NetworkName, string[]> = {
         "https://mainnet-rpc.sui.chainbase.online",
         "https://sui-mainnet-endpoint.blockvision.org",
         "https://sui-mainnet-ca-2.cosmostation.io",
-        "https://sui-mainnet-eu-3.cosmostation.io",
-        "https://sui-mainnet-eu-4.cosmostation.io",
         "https://sui-mainnet-us-1.cosmostation.io",
         "https://sui-mainnet-us-2.cosmostation.io",
-        "https://sui-mainnet-rpc.allthatnode.com",
-        "https://sui-mainnet-rpc-korea.allthatnode.com",
-        "https://sui-mainnet-rpc-germany.allthatnode.com",
 
+        // "https://sui-mainnet-rpc.allthatnode.com",           // 000
+        // "https://sui-mainnet-rpc-korea.allthatnode.com",     // 000
+        // "https://sui-mainnet-rpc-germany.allthatnode.com",   // 000
         // "https://sui.publicnode.com",                        // CORS error
         // "https://sui-mainnet-rpc.bartestnet.com",            // 403
         // 'https://sui-rpc-mainnet.testnet-pride.com',         // 502
         // 'https://sui-mainnet-ca-1.cosmostation.io',          // 502
         // 'https://sui-mainnet-eu-1.cosmostation.io',          // 000
         // 'https://sui-mainnet-eu-2.cosmostation.io',          // 000
+        // "https://sui-mainnet-eu-3.cosmostation.io",          // CORS error
+        // "https://sui-mainnet-eu-4.cosmostation.io",          // CORS error
     ],
     "testnet": [
-        getFullnodeUrl("testnet"),
+        removePort(getFullnodeUrl("testnet")),
         "https://rpc-testnet.suiscan.xyz",
         "https://sui-testnet-endpoint.blockvision.org",
         "https://sui-testnet.public.blastapi.io",
         "https://testnet.suiet.app",
-        "https://sui-testnet.nodeinfra.com",                    // 429 too many requests (occasionally)
-        // 'https://testnet.sui.rpcpool.com',                   // 403 forbidden when using VPN
+        "https://sui-testnet.nodeinfra.com",
+        'https://testnet.sui.rpcpool.com',
     ],
     "devnet": [
-        getFullnodeUrl("devnet"),
+        removePort(getFullnodeUrl("devnet")),
         "https://devnet.suiet.app",
     ],
     "localnet": [
@@ -117,4 +117,9 @@ export async function newLowLatencySuiClient({
     const results = await measureRpcLatency({endpoints, rpcRequest});
     const suiClient = new SuiClient({ url: results[0].endpoint });
     return suiClient;
+}
+
+function removePort(url: string): string
+{
+    return url.replace(/:\d+/, "");
 }
