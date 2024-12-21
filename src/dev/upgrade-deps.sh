cc885272ce00450e4e781a5078ec7f1913f0f1d0
juzybits <96890594+juzybits@users.noreply.github.com>
2024-12-21 13:29:32 -0100

    dev: add upgrade-deps.sh

---
+++ src/dev/upgrade-deps.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o nounset      # Treat unset variables as an error when substituting
+set -o errexit      # Exit immediately if any command returns a non-zero status
+set -o pipefail     # Prevent errors in a pipeline from being masked
+set -o xtrace       # Print each command to the terminal before execution
+
+PATH_LOCAL=$HOME/data/code/polymedia-suitcase
+REACT_VERSION="^18.0.0"
+
+cd $PATH_LOCAL
+pnpm upgrade --latest --recursive
+pnpm upgrade --latest -w
+
+cd $PATH_LOCAL/src/react
+pnpm add -D @types/react@$REACT_VERSION
