39558f81a69c88557bfd1150bd87e3beee71c9d9
juzybits <96890594+juzybits@users.noreply.github.com>
2025-01-01 11:53:47 -0100

    all: upgrade

--- src/dev/upgrade-deps.sh
+++ src/dev/upgrade-deps.sh
@@ -5,16 +5,16 @@ set -o errexit      # Exit immediately if any command returns a non-zero status
 set -o pipefail     # Prevent errors in a pipeline from being masked
 set -o xtrace       # Print each command to the terminal before execution
 
-PATH_LOCAL=$HOME/data/code/polymedia-suitcase
+PATH_THIS=$HOME/data/code/polymedia-suitcase
 REACT_VERSION="^18.0.0"
 
-cd $PATH_LOCAL
+cd $PATH_THIS
 pnpm up --latest --recursive
 pnpm up --latest -w
 
-cd $PATH_LOCAL/src/react
+cd $PATH_THIS/src/react
 pnpm add -D @types/react@$REACT_VERSION
 
-cd $PATH_LOCAL
+cd $PATH_THIS
 pnpm up --recursive
 pnpm up -w
