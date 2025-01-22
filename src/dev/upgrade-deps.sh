6686e0319bcfd3a3d9582ed1f2376e65967696a4
juzybits <96890594+juzybits@users.noreply.github.com>
2025-01-22 18:05:41 -0100

    all: upgrade to @mysten/sui 1.20.0

--- src/dev/upgrade-deps.sh
+++ src/dev/upgrade-deps.sh
@@ -5,16 +5,18 @@ set -o errexit      # Exit immediately if any command returns a non-zero status
 set -o pipefail     # Prevent errors in a pipeline from being masked
 set -o xtrace       # Print each command to the terminal before execution
 
-PATH_THIS=$HOME/data/code/polymedia-suitcase
+SCRIPT_DIR="$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )"
+PATH_PROJECT="$( cd "$SCRIPT_DIR/../.." && pwd )"
+
 REACT_VERSION="^18.0.0"
 
-cd $PATH_THIS
+cd $PATH_PROJECT
 pnpm up --latest --recursive
 pnpm up --latest -w
 
-cd $PATH_THIS/src/react
+cd $PATH_PROJECT/src/react
 pnpm add -D @types/react@$REACT_VERSION
 
-cd $PATH_THIS
+cd $PATH_PROJECT
 pnpm up --recursive
 pnpm up -w
