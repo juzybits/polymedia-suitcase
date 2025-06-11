b650faab0c409660eca57a92014ebf2687897548
juzybits <96890594+juzybits@users.noreply.github.com>
2025-06-11 11:46:20 +0200

    core: requestSuiFromFaucet to use v2

--- src/core/src/faucets.ts
+++ src/core/src/faucets.ts
@@ -1,22 +1,22 @@
-import { requestSuiFromFaucetV1 } from "@mysten/sui/faucet";
+import { requestSuiFromFaucetV2 } from "@mysten/sui/faucet";
 
 /**
  * Get SUI from the faucet on localnet/devnet/testnet.
  */
 export async function requestSuiFromFaucet(
-    network: "localnet"|"devnet"|"testnet",
+    network: "localnet" | "devnet" | "testnet",
     recipient: string,
 ) {
     let host: string;
     if (network == "localnet") {
-        host = "http://127.0.0.1:9123/gas";
+        host = "http://127.0.0.1:9123";
     }
     else if (network == "devnet") {
-        host = "https://faucet.devnet.sui.io/v1/gas";
+        host = "https://faucet.devnet.sui.io";
     }
     else {
-        host = "https://faucet.testnet.sui.io/v1/gas";
+        host = "https://faucet.testnet.sui.io";
     }
 
-    return requestSuiFromFaucetV1({ host, recipient });
+    return requestSuiFromFaucetV2({ host, recipient });
 }
