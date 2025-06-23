7fb89e7c674168056120558523e9b77bec85c45d
juzybits <96890594+juzybits@users.noreply.github.com>
2025-06-23 21:39:19 +0200

    jest: add moduleNameMapper

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -5,6 +5,9 @@ const baseConfig: Config.InitialOptions = {
     testEnvironment: "node",
     moduleFileExtensions: ["ts", "js", "json", "node"],
     testMatch: ["**/__tests__/**/*.test.ts"],
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1"
+    },
     // verbose: true, // 'Validation Warning: Unknown option "verbose" with value true was found.'
 };
 
